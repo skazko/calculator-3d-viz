e00048161cc24939d1bf54e7955f497cee84adab
Slava <kreeon@ya.ru>
2019-12-02 21:48:51 +0300

    add flex layers

--- src/components/SampleVisualisation/SampleVisualisation.js
+++ src/components/SampleVisualisation/SampleVisualisation.js
@@ -4,7 +4,7 @@ import './SampleVisualisation.css';
 function SampleVisualisation() {
   return (
     <div className="sample-visualisation">
-      <img src="/sample.jpg" alt="sample" />
+      <img className="sample-visualisation__image" src="/sample.jpg" alt="sample" />
     </div>
   );
 }
