98f9ef595052898d38232e90ecd48ab32b4fc84e
Slava <kreeon@ya.ru>
2020-05-05 17:49:41 +0300

    change images

--- src/components/SampleVisualisation/SampleVisualisation.js
+++ src/components/SampleVisualisation/SampleVisualisation.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './SampleVisualisation.css';
 
-function SampleVisualisation({sampleState}) {
-  const url = `sample-${sampleState}`;
-  const img = require(`./${url}.jpg`);
+function SampleVisualisation({ sampleState }) {
+  const img = require(`./${sampleState}.jpg`);
   return (
     <div className="sample-visualisation">
       <img className="sample-visualisation__image" src={img} alt="sample" />
